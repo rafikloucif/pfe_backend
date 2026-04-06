13d2c55b639674442c878843ed6219617769caf2
rafikloucif <loucifrafik219@gmail.com>
2026-04-05 20:03:40 -0700

    new structure

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const express = require('express');
 const connectDB = require('./config/db');
-const aiRoutes = require('./routes/ai.routes');
 const positionRoutes = require('./routes/position.route');
 
 const app = express();
@@ -36,7 +35,6 @@ app.use('/api/clients',     require('./routes/client.routes'));
 app.use('/api/fournisseurs',require('./routes/fournisseur.routes'));
 app.use('/api/chauffeurs',  require('./routes/chauffeur.routes'));
 app.use('/api/commandes',   require('./routes/commande.routes'));
-app.use('/api/ai',          aiRoutes);
 app.use('/api/position',    positionRoutes);
 
 const PORT = process.env.PORT || 3000;
